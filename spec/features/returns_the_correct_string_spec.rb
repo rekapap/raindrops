d9d9299427f4aad89796b853bd20a66aff163865
Reka <rekapap28@gmail.com>
2019-02-18 19:39:19 +0000

    adds feature test for multiple factors

--- spec/features/returns_the_correct_string_spec.rb
+++ spec/features/returns_the_correct_string_spec.rb
@@ -1,10 +1,13 @@
 require 'raindrops'
 
-feature 'Returns the correct string' do
+feature 'Feature: Returns the correct string' do
   scenario 'the input returns the digits if it\'s not a factorr of 3,5,7' do
     expect(Raindrops.convert(34)).to eq('34')
   end
   scenario 'for a number that is a factor of 7 it outputs \'Plong\'' do
     expect(Raindrops.convert(28)).to eq('Plong')
+  end
+  scenario 'for a number that is a factor of 30 it outputs \'PlingPlang\'' do
+    expect(Raindrops.convert(30)).to eq('PlingPlang')
   end
 end
